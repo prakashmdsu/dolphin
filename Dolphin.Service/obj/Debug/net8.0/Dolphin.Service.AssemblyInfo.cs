c5b0ce7f105074fd827ed1c20cf34da2cebc6819
prakashmdsu <prakash.dsu@gmail.com>
2025-12-12 23:04:01 +0530

    dockercompose

--- Dolphin.Service/obj/Debug/net8.0/Dolphin.Service.AssemblyInfo.cs
+++ Dolphin.Service/obj/Debug/net8.0/Dolphin.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dolphin.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659409b6f39cff0ce992abe9479213aba1407f6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f77367ca1fcba065df3df098c71e3b979be5e9ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dolphin.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dolphin.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
