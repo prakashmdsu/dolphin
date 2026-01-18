40ff625e3be6c199ad97ce4b0ff47dacc9c75de5
prakashmdsu <prakash.dsu@gmail.com>
2026-01-18 10:29:32 +0530

    issue fixes

--- Dolphin.Service/obj/Debug/net8.0/Dolphin.Service.AssemblyInfo.cs
+++ Dolphin.Service/obj/Debug/net8.0/Dolphin.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dolphin.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c434a7997dfc8c8aa74cb5be68070c477570d31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6200d344e6bae415c10961e6ae0a2eee895a4af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dolphin.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dolphin.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
